a21d9f25717abbc6c8db456eece6b0bc8f932448
Aniket <anikettripathi2708@gmail.com>
2023-06-20 18:20:42 +0530

    4th checkpoint

--- server/server.js
+++ server/server.js
@@ -1,11 +1,107 @@
 const express = require('express');
 const cors = require('cors');
 const { spawn } = require('child_process');
+const mongoose = require('mongoose');
 const app = express();
 
 // Middleware for CORS and JSON parsing
 app.use(cors());
 app.use(express.json());
+
+// Connect to MongoDB with Mongoose
+mongoose.connect("mongodb+srv://aniket:aniketdcst@cluster0.y32diki.mongodb.net/user-data?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("We're connected to the database!");
+});
+
+// Define a Mongoose schema and model for User
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  name: String,
+  age: Number,
+  Pregnancies: Number,
+  Glucose: Number,
+  BloodPressure: Number,
+  SkinThickness: Number,
+  Insulin: Number,
+  BMI: Number,
+  DiabetesPedigreeFunction: Number,
+  Diabetes: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+// CREATE a new user
+app.post('/register', async (req, res) => {
+    const newUser = new User(req.body);
+
+    try {
+        await newUser.save();
+        res.status(201).send(newUser);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// READ user profile
+app.get('/profile', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.query.username });
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
+// UPDATE user details
+app.patch('/updateProfile', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['username', 'password', 'name', 'age', 'Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Diabetes'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        const user = await User.findOne({ username: req.query.username });
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        updates.forEach((update) => user[update] = req.body[update]);
+        await user.save();
+
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// DELETE user
+app.delete('/deleteUser', async (req, res) => {
+    try {
+        const user = await User.findOneAndDelete({ username: req.query.username });
+
+        if (!user) {
+            res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(500).send();
+    }
+});
 
 app.post('/predict', (req, res) => {
     const python = spawn('python', ['./MLModels/predict.py', 
@@ -18,6 +114,7 @@ app.post('/predict', (req, res) => {
                                     req.body.DiabetesPedigreeFunction,
                                     req.body.Age]);
 
+//...
     let result = '';
 
     python.stdout.on('data', (data) => {
